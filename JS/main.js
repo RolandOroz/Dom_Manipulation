b57cc9a1b546703a6c0241985612346bbea4df80
RolandOroz <rolandoroz@gmail.com>
2022-01-28 00:12:43 +0100

    made object user

--- JS/main.js
+++ JS/main.js
@@ -9,7 +9,7 @@ let inputArr = [
         firstName: 'Marjan',
         lastName: 'Kozlevcar',
         address: 'Sterletova ulica 132',
-        dob: '1965-04-130'
+        dob: '1965-04-30'
     },
     {
         firstName: 'Hugo',
@@ -22,6 +22,30 @@ let inputArr = [
         lastName: 'Oroz',
         address: 'Schwannenstrasse 22',
         dob: '2010-10-10'
+    },
+    {
+        firstName: 'George',
+        lastName: 'Flank',
+        address: 'Thaler 234',
+        dob: '1987-02-03',
+    },
+    {
+        firstName: 'Sophie',
+        lastName: 'Taller',
+        address: 'Manddrow 88',
+        dob: '1995-04-10'
+    },
+    {
+        firstName: 'Anja',
+        lastName: 'Tweety',
+        address: 'Looney tunes 65',
+        dob: '1988-08-20'
+    },
+    {
+        firstName: 'Miki',
+        lastName: 'Maus',
+        address: 'Goofyway 2',
+        dob: '1996-11-23'
     }
 ];
 
